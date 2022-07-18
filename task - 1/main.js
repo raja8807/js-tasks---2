ce5e57e72ae45d2977689a281d6a8c9aa8ce5c27
Raja <rajarathinam1999@gmail.com>
2022-07-18 15:07:00 +0530

    18.7

---
+++ task - 1/main.js
@@ -0,0 +1,38 @@
+function numToWord(num) {
+    let word;
+    if (num == 0) {
+        word = "Zero";
+    } else if (num == 1) {
+        word = "One";
+    }else if(num==2){
+        word = "Two";
+    }else if(num==3){
+        word = "Three";
+    }else if(num==4){
+        word = "Four";
+    }else if(num==5){
+        word = "Five";
+    }else if(num==6){
+        word = "Six";
+    }else if(num==7){
+        word = "Seven";
+    }else if(num==8){
+        word = "Eight";
+    }else if(num==9){
+        word = "Nine";
+    }
+    return word;
+}
+
+let input = parseInt(prompt("Enter The Number :"));
+
+let temp = input;
+let x = " ";
+
+while(temp!=0){
+    let lastDigit = temp%10;
+    x = x+" "+numToWord(lastDigit);
+    temp = (temp - lastDigit)/10;
+}
+
+console.log(x);
